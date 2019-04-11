b96484e66e297948de3ac6f19208208ed35f8fa7
swethapremkumar <49434762+swethapremkumar@users.noreply.github.com>
2019-04-11 10:42:26 +0530

    Add files via upload

---
+++ scrummaster.php
@@ -0,0 +1,66 @@
+<html>
+<title>SCRUM MASTER LOG IN</title>
+<body>
+<style>
+table,th,td {
+	border: 1px solid black;
+	border-collapse: collapse;
+}
+th,td{
+	padding: 15px;
+}
+table, th, td {
+	border: 1px solid black;
+	border-collapse: collapse;
+}
+table{
+	border-spacing: 15px;
+}
+</style>
+
+<table style="width:100%">
+  <tr>
+	<td>S.NO</td>
+    <td>TITLE</td>
+    <td>STATUS</td>
+    <td>PRIORITY</td>
+	<td>ASSIGNED TO</td>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td></td>
+    <td></td>
+	<td></td>
+	<td></td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td></td>
+    <td></td>
+	<td></td>
+	<td></td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td></td>
+    <td></td>
+	<td></td>
+	<td></td>
+  </tr>
+  <tr>
+    <td>4</td>
+    <td></td>
+    <td></td>
+	<td></td>
+	<td></td>
+  </tr>
+  <tr>
+    <td>5</td>
+    <td></td>
+    <td></td>
+	<td></td>
+	<td></td>
+  </tr>
+</table>
+</body>
+</html>
